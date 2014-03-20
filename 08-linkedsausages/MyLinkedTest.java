4fc1cbb93147558c77cae2b6272ff488a6f7e548
Tina Zhu <tinazhu1314@yahoo.com>
2014-03-20 00:23:21 -0400

    Brakefast

---
+++ 08-linkedsausages/MyLinkedTest.java
@@ -0,0 +1,13 @@
+public class MyLinkedTest{
+    public static void main(String[]args){
+	MyLinkedList mine = new MyLinkedList();
+	mine.add("Sullen");
+	mine.add("Gustaf");	
+	System.out.println(mine);
+	mine.add("Shelly");
+	System.out.println(mine);
+	mine.add("Bart",1);
+	System.out.println(mine);
+
+    }
+}
